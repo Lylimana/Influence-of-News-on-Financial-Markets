058faeeb4c73641457d66d4356cea33f3201abbf
Miguel Manalili <manalili.mig@gmail.com>
2025-10-07 17:16:58 +0100

    Exporting Data Test

--- Financial Modeling Prep Dataset.py
+++ Financial Modeling Prep Dataset.py
@@ -1,14 +1,14 @@
 from fmp_python.fmp import FMP
 import certifi
 import json
+import pandas as pd
 
+# Stock Symbol Search API
 url = "https://financialmodelingprep.com/stable/search-symbol?query=AAPL&apikey=F4h1wP95SrsrfPZT6M28hUdUxIYYX27q"
 
 try:
-    # For Python 3.0 and later
     from urllib.request import urlopen
 except ImportError:
-    # Fall back to Python 2's urllib2
     from urllib2 import urlopen
 
 def get_jsonparsed_data(url):
@@ -16,4 +16,9 @@ def get_jsonparsed_data(url):
     data = response.read().decode("utf-8")
     return json.loads(data)
 
-print(get_jsonparsed_data(url))
+AAPL = get_jsonparsed_data(url)
+
+df = pd.DataFrame(AAPL)
+
+# Exporting Data
+df.to_excel('FMP Dataset.xlsx', index = False)
